6082f046acc48a2ef834c684126d28d116f2cbda
UENO Yuuji <uenoyuuji@gmail.com>
2012-11-13 23:34:05 +0900

    replace jasmine to mocha + chai

--- test/commands.js
+++ test/commands.js
@@ -1,16 +1,19 @@
+var expect = require('chai').expect;
+var assert = require('chai').assert;
+
 describe('commands', function() {
     var define = require('../commands').define;
 
     it('should has a function named define', function() {
-        expect(typeof(define)).toBe('function');
+        expect(define).to.be.a('function');
     });
 
     describe('define', function() {
         it('should returns an object that has a function named apply', function() {
             var definedCommands = define();
-            expect(definedCommands).not.toBeNull();
-            expect(typeof(definedCommands)).toBe('object');
-            expect(typeof(definedCommands.apply)).toBe('function');
+            expect(definedCommands).not.to.be.null;
+            expect(definedCommands).to.be.a('object');
+            expect(definedCommands.apply).to.be.a('function');
         });
 
         describe('apply', function() {
@@ -21,75 +24,73 @@ describe('commands', function() {
                 var commandCalled = 0;
                 var definedCommands = define({
                     command1: function(arg1, arg2) {
-                        expect(arg1).toBe(process.argv[3]);
-                        expect(arg2).toBe(process.argv[4]);
+                        expect(arg1).to.equal(process.argv[3]);
+                        expect(arg2).to.equal(process.argv[4]);
                         commandCalled++;
                     },
                     command2: function() {
-                        fail(Error('Assertion failure: unexpected command function called'));
+                        assert.fail(null, null, 'unexpected command function called');
                     }
                 });
-                expect(commandCalled).toBe(0);
+                expect(commandCalled).to.equal(0);
 
                 var applyCalled = 0;
                 definedCommands.apply(function(applied, command, args) {
-                    expect(applied).toBe(true);
-                    expect(command).toBe(process.argv[2]);
-                    expect(Array.isArray(args)).toBe(true);
-                    expect(args[0]).toBe(process.argv[3]);
-                    expect(args[1]).toBe(process.argv[4]);
+                    expect(applied).to.be.true;
+                    expect(command).to.equal(process.argv[2]);
+                    expect(Array.isArray(args)).to.be.true;
+                    expect(args[0]).to.equal(process.argv[3]);
+                    expect(args[1]).to.equal(process.argv[4]);
                     applyCalled++;
                 });
-                expect(commandCalled).toBe(1);
-                expect(applyCalled).toBe(1);
+                expect(commandCalled).to.equal(1);
+                expect(applyCalled).to.equal(1);
             });
 
             it('missing command', function() {
                 process.argv = ['node', 'file'];
 
-                var fail = this.fail;
                 var definedCommands = define({
                     command1: function() {
-                        fail(Error('Assertion failure: unexpected command function called'));
+                        assert.fail(null, null, 'unexpected command function called');
                     },
                     command2: function() {
-                        fail(Error('Assertion failure: unexpected command function called'));
+                        assert.fail(null, null, 'Assertion failure: unexpected command function called');
                     }
                 });
 
                 var applyCalled = 0;
                 definedCommands.apply(function(applied, command, args) {
-                    expect(applied).toBe(false);
-                    expect(command).toBeFalsy();
-                    expect(Array.isArray(args)).toBe(true);
-                    expect(args.length).toBe(0);
+                    expect(applied).to.be.false;
+                    expect(command).not.to.be.a('string');
+                    expect(Array.isArray(args)).to.be.true;
+                    expect(args).to.have.length(0);
                     applyCalled++;
                 });
-                expect(applyCalled).toBe(1);
+                expect(applyCalled).to.equal(1);
             });
 
             it('undefined command', function() {
                 process.argv = ['node', 'file', 'command3'];
 
-                var fail = this.fail;
                 var definedCommands = define({
                     command1: function() {
-                        fail(Error('Assertion failure: unexpected command function called'));
+                        assert.fail(null, null, 'unexpected command function called');
                     },
                     command2: function() {
-                        fail(Error('Assertion failure: unexpected command function called'));
+                        assert.fail(null, null, 'unexpected command function called');
                     }
                 });
 
                 var applyCalled = 0;
                 definedCommands.apply(function(applied, command, args) {
-                    expect(applied).toBe(false);
-                    expect(command).toBe(process.argv[2]);
-                    expect(Array.isArray(args)).toBe(true);
-                    expect(args.length).toBe(0);
+                    expect(applied).to.equal(false);
+                    expect(command).to.equal(process.argv[2]);
+                    expect(Array.isArray(args)).to.be.true;
+                    expect(args).to.have.length(0);
                     applyCalled++;
                 });
-                expect(applyCalled).toBe(1);
+                expect(applyCalled).to.equal(1);
             });
         });
     });
